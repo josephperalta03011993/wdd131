169516c231f24d3f0d9d8cd3dbbc609d6b1c944f
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-03-07 19:24:55 +0800

    fix css swap

--- week01/scripts/getdates.js
+++
@@ -1,9 +0,0 @@
-let year = document.querySelector("#currentyear");
-let lastModified = document.querySelector("#lastModified");
-const today = new Date();
-
-// Get the current year and display it in the footer
-year.innerHTML = `&#169; ${today.getFullYear()}`;
-
-// Get last modified date of the document and display it in the footer
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
