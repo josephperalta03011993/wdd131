891cf5a410b4921194f74bacabcddce52c087bc3
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-03-14 15:25:53 +0800

    temple grid responsive view

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,11 +1,26 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-deleteButton.ariaLabel = 'Remove Verse';
-li.append(deleteButton);
-list.append(li);
+button.addEventListener('click', () => {
+    if (input.value.trim() !== '') {
+
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        deleteButton.ariaLabel = 'Remove Verse';
+        
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+    }
+});
