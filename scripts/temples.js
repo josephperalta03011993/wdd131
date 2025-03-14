3ef1887aef5edf5f0cbe5ed302fed11d5f87b643
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-03-14 17:09:55 +0800

    complete

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,3 +7,12 @@ year.innerHTML = `&#169; ${today.getFullYear()}`;
 
 // Get last modified date of the document and display it in the footer
 lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+// Hamburger Menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
