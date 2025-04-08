621d073c0ac94adaf8aac6777f63452fe899dcbd
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-04-09 00:59:44 +0800

    update color array prod list

--- scripts/form.js
+++ scripts/form.js
@@ -1,9 +1,31 @@
 // Product Array
 const products = [
-    { id: "p1", name: "Product 1" },
-    { id: "p2", name: "Product 2" },
-    { id: "p3", name: "Product 3" }
-];
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
 
 // Populate Product Name Select
 function populateProducts() {
